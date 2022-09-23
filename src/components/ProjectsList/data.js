e1870ed2645757370d8b58f07b3e4a9e07f75512
Sunny Kumar <sunnymahto9824@mail.com>
2022-09-23 09:11:05 +0530

    Heloo

--- src/components/ProjectsList/data.js
+++ src/components/ProjectsList/data.js
@@ -6,12 +6,17 @@ import lambo from "../img/lamborghini.jpg"
 import tic from "../img/tic-tac-toe.jpg"
 import action from "../img/action.png"
 
- const Data=[
-    {img:cal,title:'Calculator',desc:'It is a basic Calculator which perform all basic operation on an expression'},
-    {img:clock,title:'Analog Clock',desc:'This is an analog clock which shows the current time according to its PC or Computer'},
-    {img:lambo,title:'Moving Car',desc:'It is a moving Lamborghini car which is moving forward with constant speed.'},
-    {img:tic,title:'Tic-Tac-Toe',desc:'It is a paper-and-pencil game for two players who take turns marking the spaces ina threeby-three grid with X or O.'},
-    {img:action,title:'Dragon Action Game',desc:'It is a Dragon Action Game which goes upward,downward,backward and forward onpressing the up arrow,down arrow,back arrow and right arrow respectively.'},
-    {img:p4,title:'DownSyndrome Detection',desc:'It is project to detect a person is suffering from downsyndrome or not using facialphotographs.'},
+const Data = [
+    {
+        img: cal,
+        title: 'Calculator',
+        desc: 'It is a basic Calculator which perform all basic operation on an expression'
+    },
+
+    { img: clock, title: 'Analog Clock', desc: 'This is an analog clock which shows the current time according to its PC or Computer' },
+    { img: lambo, title: 'Moving Car', desc: 'It is a moving Lamborghini car which is moving forward with constant speed.' },
+    { img: tic, title: 'Tic-Tac-Toe', desc: 'It is a paper-and-pencil game for two players who take turns marking the spaces ina threeby-three grid with X or O.' },
+    { img: action, title: 'Dragon Action Game', desc: 'It is a Dragon Action Game which goes upward,downward,backward and forward onpressing the up arrow,down arrow,back arrow and right arrow respectively.' },
+    { img: p4, title: 'DownSyndrome Detection', desc: 'It is project to detect a person is suffering from downsyndrome or not using facialphotographs.' },
 ]
 export default Data;
